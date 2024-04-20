7d4914ea0ce90f33955efc544451bd5eb599c86c
borgra <steveborgra@gmail.com>
2024-04-19 20:24:27 -0500

    Added User Table and Seed Data for AccountType

--- WealthMgmt/WealthMgmt.Infrastructure.Database/Pre-Deployment/Seed_AccountType.sql
+++ WealthMgmt/WealthMgmt.Infrastructure.Database/Pre-Deployment/Seed_AccountType.sql
@@ -1,11 +1,18 @@
-﻿/*
-Post-Deployment Script Template							
---------------------------------------------------------------------------------------
- This file contains SQL statements that will be appended to the build script.		
- Use SQLCMD syntax to include a file in the post-deployment script.			
- Example:      :r .\myfile.sql								
- Use SQLCMD syntax to reference a variable in the post-deployment script.		
- Example:      :setvar TableName MyTable							
-               SELECT * FROM [$(TableName)]					
---------------------------------------------------------------------------------------
-*/
+﻿PRINT 'Seed Table: dbo.AccountType';
+
+MERGE INTO dbo.AccountType AS target
+USING (VALUES 
+    (1, 'Savings Account', 1),
+    (2, 'Checking Account',1),
+    (3, '401k', 1),
+    (4, 'IRA', 1),
+    (4, 'ROTH IRA', 1),
+    (4, 'Pension', 1)
+) AS source (AccountTypeId, AccountTypeName, IsActive)
+ON target.AccountTypeId = source.AccountTypeId
+WHEN MATCHED THEN
+    UPDATE SET target.AccountTypeName = source.AccountTypeName, target.IsActive = source.IsActive
+WHEN NOT MATCHED BY TARGET THEN
+    INSERT (AccountTypeId, AccountTypeName, IsActive) VALUES (source.AccountTypeId, source.AccountTypeName, source.IsActive);
+
+PRINT 'Seed Complete: dbo.AccountType';
---
+++ WealthMgmt/WealthMgmt.Infrastructure.Database/Table/User.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] NVARCHAR(50) NULL, 
+    [LastName] NVARCHAR(50) NULL, 
+    [DateOfBirth] DATE NULL, 
+    [Email] NVARCHAR(255) NULL, 
+    [IsActive] BIT NULL DEFAULT 1
+)
