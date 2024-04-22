fe63503bfc87765a2c2babc233e4b4ca6a705ed8
borgra <steveborgra@gmail.com>
2024-04-21 22:23:55 -0500

    Added New Tables for AccountAttribute and UserAccount

---
+++ WealthMgmt/WealthMgmt.Infrastructure.Database/Table/AccountAttribute.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[AccountAttribute]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [AccountTypeId] INT NOT NULL,
+    [Name] NVARCHAR(255) NOT NULL, 
+    [Value] NVARCHAR(255) NOT NULL
+)
---
+++ WealthMgmt/WealthMgmt.Infrastructure.Database/Table/UserAccount.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[UserAccount]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(255) NOT NULL,
+    [Balance] MONEY NOT NULL DEFAULT 0,
+    [UserId] INT NOT NULL, 
+    [AccountTypeId] INT NOT NULL
+)
