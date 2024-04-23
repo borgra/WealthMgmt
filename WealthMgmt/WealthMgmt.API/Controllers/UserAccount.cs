7481b076692ec06eb740e5d1f64276a1b9e6713a
borgra <steveborgra@gmail.com>
2024-04-23 13:10:24 -0500

    Added new controllers and placeholders

---
+++ WealthMgmt/WealthMgmt.API/Controllers/UserAccount.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WealthMgmt.API.Controllers
+{
+    public class UserAccount : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
