d6ce3ed9378136bd34e4ba6bc0cf0441dc993c60
borgra <steveborgra@gmail.com>
2024-04-22 21:26:47 -0500

    Start of Domain Layer

---
+++ WealthMgmt/WealthMgmt.Core.Domain/Entities/Account/RetirementAccount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WealthMgmt.Core.Domain.Entities.Account
+{
+    public  class RetirementAccount: AccountBase
+    {
+        public RetirementAccount() { }
+    }
+}
