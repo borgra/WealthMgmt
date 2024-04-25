cc07036110fd22968b16f0f9aaa2078821eec846
borgra <steveborgra@gmail.com>
2024-04-25 16:06:47 -0500

    Playing around

---
+++ WealthMgmt/WealthMgmt.API/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WealthMgmt.Core.Application.DTO;
+
+namespace WealthMgmt.API.Controllers
+{
+    //[Route("[controller]/v1")]
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //[Route("/Account/{id}")]
+        //public async Task<ActionResult<AccountDTO>> GetAccountAsync(int id)
+        //{
+        //    return null;
+        //}
+
+    }
+}
