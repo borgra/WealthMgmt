1862fa13476c8f70c8f9dd2d09b2942100a0eab6
borgra <steveborgra@gmail.com>
2024-04-26 18:55:22 -0500

    poc

---
+++ WealthMgmt/WealthMgmt.POC.ConsoleApp/Model/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WealthMgmt.POC.ConsoleApp.Model
+{
+    internal class Account
+    {
+        string Name { get; set; }
+
+    }
+}
